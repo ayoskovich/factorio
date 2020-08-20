f7c6250bcb2645308c7d2670e6e887a9dc0a09b8
anthony <yoskovia@mail.gvsu.edu>
2020-08-20 16:47:08 -0400

    Function will now only return items per second from recipe.

---
+++ src/calc.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def ips(c_time, c_speed):
+    """ Computes items per second from a recipe and 
+        assembler.
+    
+    c_time (float): Crafting time of recipe
+    c_speed (float): Crafting speed of the assembler
+    """
+    return 1 / (c_time / c_speed)
+
+
+# In[2]:
+
+
+ips(c_time=3.2, c_speed=2)
+
